69c94bf7a3b0695bde30fe24cacd43ae92e2d11e
Russ93 <russ93@fullsail.edu>
2013-12-03 14:57:30 -0500

    Lab on2 2nd commit

---
+++ Lab_Work/Lab_1/5_pages/app/j/m.js
@@ -0,0 +1,23 @@
+(function(){
+window.onload = function (){
+	var tabs = $('aside ul li')
+	$('section textarea').css('height',$(window).height()-152);
+	$('aside').css('height',$(window).height()-92);
+	for(var i=0;i<tabs.length;i++){
+		$($(tabs[i]).children('p')).html($(tabs[i]).children('p').text().substr(0, 250))
+		$($(tabs[i]).children('div')).click(function(){
+			var c = true;
+			
+			if(c){
+				$(this).prepend('Sure?<span>No</span><span>Yes</span>');
+				c = false;
+			}else{
+				$(this).empty();
+			}
+			console.log(c)
+			$(this).children('span:contains(delete)').remove()
+			$(this).children('span:contains(No)').css('color','#999');
+			$(this).children('span:contains(No)').click(function(){});
+		})//tabs[i].div.click()
+	}// for tabs
+}})();
